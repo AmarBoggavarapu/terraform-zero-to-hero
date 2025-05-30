8fef96560c54cc2206c72433abbce2901c16784c
AmarBoggavarapu <121476723+AmarBoggavarapu@users.noreply.github.com>
2025-05-30 12:04:42 -0400

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "amarbvn-remote-backend-s3" # change this
-    key            = "amarbvn//terraform.tfstate"
+    key            = "amarbvn/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
