0d82409460776b19ce0910598e35ca1e8587472c
AmarBoggavarapu <121476723+AmarBoggavarapu@users.noreply.github.com>
2025-06-02 23:42:44 +0000

    Updated main.tf

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,30 +3,34 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://54.89.99.162:8200" ## Public IP address of EC2 instance: Port (8200 used in creating vault configuration)
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "XXXXXXXXXXXXXX" ## Role ID created in vault integration setup
+      secret_id = "XXXXXXXXXXXXXX" ## Secret ID created in vault integration setup.
+      ## secret_id is often one-time use unless configured otherwise.
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
+  mount = "kv" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
 
+
 resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-084568db4383264d4"   ## Update accordingly as per your machine
   instance_type = "t2.micro"
+  subnet_id = "subnet-0d9f444378a9b2e69"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Name = "test-ec2-instance"
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
+
